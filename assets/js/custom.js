a9f907a40beb0b4c2b39b95ff2027ce1ea1c5a9a
strongdever <kstronger77@gmail.com>
2023-04-07 10:56:53 +0800

    202304071056

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,84 +1,27 @@
-//inquiry-purpose Combobox start-------------
-    var x, i, j, l, ll, selElmnt, a, b, c;
-    /*look for any elements with the class "inquiry-purpose":*/
-    x = document.getElementsByClassName("inquiry-purpose");
-    l = x.length;
+//Usecase Tab bar start-----------------
 
-    for (i = 0; i < l; i++) {
-    selElmnt = x[i].getElementsByTagName("select")[0];
-    ll = selElmnt.length;
-    /*for each element, create a new DIV that will act as the selected item:*/
-    a = document.createElement("DIV");
-    a.setAttribute("class", "select-selected");
-    a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
-    x[i].appendChild(a);
-    /*for each element, create a new DIV that will contain the option list:*/
-    b = document.createElement("DIV");
-    b.setAttribute("class", "select-items select-hide");
+function openUsecase(evt, usecaseName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tab-content");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
 
-    for (j = 1; j < ll; j++) {
-        /*for each option in the original select element,
-        create a new DIV that will act as an option item:*/
-        c = document.createElement("DIV");
-        c.innerHTML = selElmnt.options[j].innerHTML;
-        c.addEventListener("click", function(e) {
-            /*when an item is clicked, update the original select box,
-            and the selected item:*/
-            var y, i, k, s, h, sl, yl;
-            s = this.parentNode.parentNode.getElementsByTagName("select")[0];
-            sl = s.length;
-            h = this.parentNode.previousSibling;
-            for (i = 0; i < sl; i++) {
-            if (s.options[i].innerHTML == this.innerHTML) {
-                s.selectedIndex = i;
-                h.innerHTML = this.innerHTML;
-                y = this.parentNode.getElementsByClassName("same-as-selected");
-                yl = y.length;
-                for (k = 0; k < yl; k++) {
-                y[k].removeAttribute("class");
-                }
-                this.setAttribute("class", "same-as-selected");
-                break;
-            }
-            }
-            h.click();
-        });
-        b.appendChild(c);
-    }
-    x[i].appendChild(b);
-    a.addEventListener("click", function(e) {
-        /*when the select box is clicked, close any other select boxes,
-        and open/close the current select box:*/
-        e.stopPropagation();
-        closeAllSelect(this);
-        this.nextSibling.classList.toggle("select-hide");
-        this.classList.toggle("select-arrow-active");
-        });
-    }
-
-    function closeAllSelect(elmnt) {
-        /*a function that will close all select boxes in the document,
-        except the current select box:*/
-        var x, y, i, xl, yl, arrNo = [];
-        x = document.getElementsByClassName("select-items");
-        y = document.getElementsByClassName("select-selected");
-        xl = x.length;
-        yl = y.length;
-        for (i = 0; i < yl; i++) {
-            if (elmnt == y[i]) {
-            arrNo.push(i)
-            } else {
-            y[i].classList.remove("select-arrow-active");
-            }
-        }
-        for (i = 0; i < xl; i++) {
-            if (arrNo.indexOf(i)) {
-            x[i].classList.add("select-hide");
-            }
-        }
-    }
-    /*if the user clicks anywhere outside the select box,
-    then close all select boxes:*/
-    document.addEventListener("click", closeAllSelect);
-
-//inquiry-purpose Combobox end-------------
+  if(usecaseName == "usecase1"){
+    document.getElementById(usecaseName).style.backgroundColor = "#087AC9";
+  }
+  if(usecaseName == "usecase2"){
+    document.getElementById(usecaseName).style.backgroundColor = "#3CA1E1";
+  }
+  if(usecaseName == "usecase3"){
+    document.getElementById(usecaseName).style.backgroundColor = "#59BAF0";
+  }
+  tablinks = document.getElementsByClassName("tabs-button");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  var targetcontent = document.getElementById(usecaseName);
+  targetcontent.style.display = "flex";  
+  evt.currentTarget.className += " active";
+}
+//Usecase Tab bar start-----------------
