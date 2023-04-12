6184dd5a3285cde55ba43fd2ca6eae87fc1d7b70
strongdever <kstronger77@gmail.com>
2023-04-12 23:18:28 +0800

    202304130018

--- wp-content/themes/twentytwentyone-child/page.php
+++ wp-content/themes/twentytwentyone-child/page.php
@@ -5,6 +5,9 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Orb</title>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="shortcut icon" href="<?php echo get_theme_file_uri(); ?>/assets/images/logo/favicon.png">
+    <link rel="icon" type="image/png" href="<?php echo get_theme_file_uri(); ?>/assets/images/logo/favicon.png">
+    <link rel="apple-touch-icon" type="image/png" href="<?php echo get_theme_file_uri(); ?>/assets/images/logo/favicon.png">
 	<link rel="stylesheet" href="<?php echo get_theme_file_uri(); ?>/assets/css/style.css">
 	<script src="<?php echo get_theme_file_uri(); ?>/assets/js/custom.js"></script>
 </head>
