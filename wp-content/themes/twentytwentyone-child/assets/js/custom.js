a582a860f2c8b712d72a08e9f26b3c1e5d1ecc26
strongdever <kstronger77@gmail.com>
2023-04-17 00:59:21 +0800

    202304170200

--- wp-content/themes/twentytwentyone-child/assets/js/custom.js
+++ wp-content/themes/twentytwentyone-child/assets/js/custom.js
@@ -9,12 +9,36 @@ function openUsecase(evt, usecaseName) {
 
   if(usecaseName == "usecase1"){
     document.getElementById(usecaseName).style.backgroundColor = "#087AC9";
+    if(window.innerWidth < 650){
+      document.getElementsByClassName('tab1')[0].style.width = "193px";
+      document.getElementsByClassName('tab1')[0].childNodes[0].style.display = "inline";
+      document.getElementsByClassName('tab2')[0].style.width = "71px";
+      document.getElementsByClassName('tab2')[0].childNodes[0].style.display = "none";
+      document.getElementsByClassName('tab3')[0].style.width = "71px";
+      document.getElementsByClassName('tab3')[0].childNodes[0].style.display = "none";
+    }
   }
   if(usecaseName == "usecase2"){
-    document.getElementById(usecaseName).style.backgroundColor = "#3CA1E1";
+      document.getElementById(usecaseName).style.backgroundColor = "#3CA1E1";
+    if(window.innerWidth < 650){
+      document.getElementsByClassName('tab1')[0].style.width = "71px";
+      document.getElementsByClassName('tab1')[0].childNodes[0].style.display = "none";
+      document.getElementsByClassName('tab2')[0].style.width = "193px";
+      document.getElementsByClassName('tab2')[0].childNodes[0].style.display = "inline";
+      document.getElementsByClassName('tab3')[0].style.width = "71px";
+      document.getElementsByClassName('tab3')[0].childNodes[0].style.display = "none";
+    }
   }
   if(usecaseName == "usecase3"){
-    document.getElementById(usecaseName).style.backgroundColor = "#59BAF0";
+      document.getElementById(usecaseName).style.backgroundColor = "#59BAF0";
+    if(window.innerWidth < 650){
+      document.getElementsByClassName('tab1')[0].style.width = "71px";
+      document.getElementsByClassName('tab1')[0].childNodes[0].style.display = "none";
+      document.getElementsByClassName('tab2')[0].style.width = "71px";
+      document.getElementsByClassName('tab2')[0].childNodes[0].style.display = "none";
+      document.getElementsByClassName('tab3')[0].style.width = "193px";
+      document.getElementsByClassName('tab3')[0].childNodes[0].style.display = "inline";
+    }
   }
   tablinks = document.getElementsByClassName("tabs-button");
   for (i = 0; i < tablinks.length; i++) {
@@ -24,14 +48,69 @@ function openUsecase(evt, usecaseName) {
   targetcontent.style.display = "flex";  
   evt.currentTarget.className += " active";
 }
-//Usecase Tab bar start-----------------
 
-//News Popup Start----------------------
+function initial(){
+  if(window.pageYOffset < 98){
+    document.getElementById("header").style.top = "0";
+  }
+}
+
+//Hiding and showing header when scrolling down and up-------start----------
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var screen_width = screen.width;
+  var currentScrollPos = window.pageYOffset;
+  if (screen_width > 650){
+    console.log(screen_width);
+    console.log(screen_width);
+    if (currentScrollPos < 10) {
+      document.getElementById("header").style.top = "0";
+    } else {
+      document.getElementById("header").style.top = "-98px";
+    }
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+function headerVisible(event){
+  let y = event.clientY;
+  if(screen.width > 650){
+    if(y < 98){
+      document.getElementById("header").style.top = "0";
+    }
+    else{
+      if(prevScrollpos > 2){
+        console.log("cursor test");
+        document.getElementById("header").style.top = "-98px";
+      }
+    }
+  }
+}
+//Hiding and showing header when scrolling down and up-------end----------
+
+function navFuc(){
+  var x = document.getElementById("header");
+  if (x.className === "header") {
+    x.className += " responsive";
+  }
+}
+
+function navClose(){
+  var x = document.getElementById("header");
+  if (x.className === "header responsive") {
+    x.className = "header";
+  }
+}
+
+function forResponsive(){
+  var x = document.getElementById("header");
+  x.className = "header";
+}
 
 $(document).ready(function(){
 
   // When the user clicks the button, open the modal 
-  $("#btn-news-category").click(function() {
+  $(".news-category").click(function() {
     $("#news-popup-overlay")[0].style.display = "block";
     $("body").css('overflow', "hidden");
   });
--- wp-content/themes/twentytwentyone-child/functions.php
+++ wp-content/themes/twentytwentyone-child/functions.php
@@ -9,3 +9,31 @@ add_shortcode('url', function ( $atts ) {
 
 remove_filter('term_description','wpautop');
 remove_filter( 'the_content', 'wpautop' );
+
+add_shortcode('news', function ( $atts ) {
+    $num = ( isset( $atts['num'] ) ? $atts['num'] : -1 );
+    query_posts( array( 'posts_per_page' => $num ) );
+    ob_start();
+    while ( have_posts() ) :
+        the_post(); ?>
+        <div class="news-category" id="btn-news-category">
+            <div class="section-item">
+                <h4><?php echo date('Y年m月d日', strtotime( get_the_date() ) ); ?></h4>
+                <div class="news-category-group">
+                    <?php
+                    $cats = get_the_category();
+                    if ( $cats ) { 
+                        foreach ( $cats as $cat ) { ?>
+                            <div class="category"><?php echo $cat->name; ?></div>
+                        <?php
+                        }
+                    } ?>
+                </div>
+                <div class="news-article"><?php the_title(); ?></div>
+                <div class="news-content" style="display:none"><?php the_content(); ?></div>
+            </div>
+        </div>
+    <?php
+    endwhile;
+    return ob_get_clean();
+} );
